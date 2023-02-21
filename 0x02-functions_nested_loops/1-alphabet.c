9da8ea929734ac1c5ea419856fa3d8dfc943a55a
deekei254 <danngarikimotho@gmail.com>
2023-02-21 02:03:48 -0800

    updates

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - alphabets in lower case
+ * Return: 0 if successful
+ */
+void print_alphabet(void)
+{
+char alph;
+for (alph = 'a'; alph <= 'z'; alph++)
+_putchar(alph);
+_putchar('\n');
+}
