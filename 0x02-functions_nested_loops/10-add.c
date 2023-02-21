e3dcc1a223e94aeff1e49502eb2a8f7c524b9bbd
deekei254 <danngarikimotho@gmail.com>
2023-02-21 13:12:11 -0800

    updates

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - prints addition of two numbers
+ * @q: value one
+ * @w: value two
+ * Return: sum of two numbers
+ */
+
+int add(int q, int w)
+{
+int a;
+a = q + w;
+return (a);
+}
