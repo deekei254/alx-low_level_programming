b97bdfdf539ba0c06dee6bb2cd44910fc9ff7dd1
deekei254 <danngarikimotho@gmail.com>
2023-02-23 10:16:53 -0800

    debugging updates

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints if input is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - infinity loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+ * 	while (1 < 10)
+ * 	{
+ * 	putchar(i);
+ * 	}
+ */
+printf("infinite loop avoided! \\o/\n");
+
+return (0);
+}
