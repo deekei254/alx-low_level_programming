bab8bf25424af85ebc7d796e85f37f283db9a331
deekei254 <danngarikimotho@gmail.com>
2023-03-07 18:21:55 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/***
+/**
  * _strspn - gets the length of a prefix substring
  * @s: pointer to a char, string that will be checked
  * @accept: pointer to the substring we check for
