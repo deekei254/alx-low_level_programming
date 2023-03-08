275d6517bd069e78ee285d5cb0b7cd60f591325d
deekei254 <danngarikimotho@gmail.com>
2023-03-07 17:23:05 -0800

    1=memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - memset function
+ * @dest: start point of string to change
+ * @src: value that will replace
+ * @n: number of bytes to change
+ * Return: change pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int y = 0;
+	int t = n;
+
+	for(;y < t; y++)
+	{
+		dest[y] = src[y];
+		n--;
+	}
+	return (dest);
+}
